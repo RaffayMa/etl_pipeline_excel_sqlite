2a82d020df84a52e944647f831668a46b063658d
RaffayMa <muhammadraffay354@gmail.com>
2025-09-22 22:41:06 -0400

    CSV loaded

--- etl_pipeline_excel_sqlite/scrpit.py
+++ etl_pipeline_excel_sqlite/scrpit.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import sqlite3 as sql
 
 # Load the data from the csv
-df = pd.read_csv('messy-data.csv')
+df = pd.read_csv('/Users/muhammadraffaymagoon/Desktop/Projects/etl_pipeline_excel_sqlite/messy-data.csv')
+
 # Examine shape
-print(df.shape)
+print(df.head())
 
